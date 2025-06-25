8b46029fd22662a22eca481edbdefe5c72d2fbb4
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-06-25 17:29:43 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,12 @@
 from parrot import parrot
 
 if __name__ == '__main__':
+    # Example calls to test the function
+    print("Calling parrot with default:")
+    parrot()
+
+    print("\nCalling parrot with a custom message:")
+    parrot("Polly wants a cracker!")
+
+    # Optional debugging session
     import ipdb; ipdb.set_trace()
